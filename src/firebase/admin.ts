ec5a0f8ffbc8ebecee357bb97fbbaccdf4d03c00
Tao119 <tao.dama.art@gmail.com>
2024-05-21 19:53:13 +0900

    fix

--- src/firebase/admin.ts
+++ src/firebase/admin.ts
@@ -1,11 +1,15 @@
+import { FFSA_PRIVATE_KEY, FSA_CLIENT_EMAIL, FSA_PROJECT_ID } from "@/constant/env";
 import { initializeApp, cert, getApps } from "firebase-admin/app";
 import { getAuth } from "firebase-admin/auth";
 
-const serviceAccount = require("/firebaseSecretKey.json");
 export const firebaseAdmin =
     getApps()[0] ??
     initializeApp({
-        credential: cert(serviceAccount),
+        credential: cert({
+            projectId: FSA_PROJECT_ID,
+            privateKey: FFSA_PRIVATE_KEY.replace(/\\n/g, '\n'),
+            clientEmail: FSA_CLIENT_EMAIL,
+        }),
     });
 
 export const auth = getAuth();
