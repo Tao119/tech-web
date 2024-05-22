fadd05e69cd1ca71755d0cc29510e639c4c3a5bb
Tao119 <tao.dama.art@gmail.com>
2024-05-23 01:12:08 +0900

    snake

--- src/firebase/admin.ts
+++ src/firebase/admin.ts
@@ -1,15 +1,16 @@
 import { FFSA_PRIVATE_KEY, FSA_CLIENT_EMAIL, FSA_PROJECT_ID } from "@/constant/env";
+import { convertToSnakeCase } from "@/services/convert";
 import { initializeApp, cert, getApps } from "firebase-admin/app";
 import { getAuth } from "firebase-admin/auth";
 
 export const firebaseAdmin =
     getApps()[0] ??
     initializeApp({
-        credential: cert({
+        credential: cert(convertToSnakeCase({
             projectId: FSA_PROJECT_ID,
             privateKey: FFSA_PRIVATE_KEY.replace(/\\n/g, '\n'),
             clientEmail: FSA_CLIENT_EMAIL,
-        }),
+        })),
     });
 
 export const auth = getAuth();
