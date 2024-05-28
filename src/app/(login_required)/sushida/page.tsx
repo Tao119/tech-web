a966a5af255cec461ed452d1698579c305cdf226
Tao119 <tao.dama.art@gmail.com>
2024-05-28 19:50:01 +0900

    link

--- src/app/(login_required)/sushida/page.tsx
+++ src/app/(login_required)/sushida/page.tsx
@@ -7,6 +7,7 @@ import SaveResult from "./save";
 import Stats from "./stats";
 import Ranking from "./ranking";
 import { SushidaData, readSushidaDataByUserId } from "@/models/sushida";
+import Link from "next/link";
 
 enum SushidaMenu {
   "結果",
@@ -42,7 +43,18 @@ const Page = () => {
 
   return (
     <div className="p-sushida">
-      <span className="p-sushida__title">寿司打</span>
+      <span className="p-sushida__title">
+        寿司打 (
+        <Link
+          href="https://sushida.net/play.html"
+          target="_brank"
+          className="p-sushida__title-link"
+        >
+          sushida.net
+        </Link>
+        )
+      </span>
+
       <div className="p-sushida__content">
         <Switcher
           className="p-sushida__switcher"
